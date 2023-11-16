4a7ffa6fadc5dade8409b21262c3e2e0c9e49871
Abhibtm <Abhisinghbtm@gmail.com>
2023-11-16 10:24:27 +0530

    Eng2 FirstCommit

---
+++ DemoV/src/test/java/TestCases/Tc2.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	
+	@Test
+	public void m2()
+	{
+		Reporter.log("Tc2 Running",true);
+	}
+
+}
