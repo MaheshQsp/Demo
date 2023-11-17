eb91760f4f6ecb5e5465b84acb00d8fc0462f79f
Abhibtm <Abhisinghbtm@gmail.com>
2023-11-17 10:23:05 +0530

    Eng1 ForthCommit

--- DemoV/src/test/java/TestCases/Tc4.java
+++ DemoV/src/test/java/TestCases/Tc4.java
@@ -1,5 +1,15 @@
 package TestCases;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Tc4 {
+	
+	@Test
+	public void tc4()
+	{
+	  Reporter.log("Tc5 Running",true);
+	  
+	}
 
 }
